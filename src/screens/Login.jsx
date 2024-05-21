0977e79d94e124f91d5140c17f15524409e031bc
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-05-21 22:04:33 +0530

    add icons

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,8 +1,11 @@
+import GoogleIcon from "../assets/google.svg";
+import FacebookIcon from "../assets/facebook.svg"
+
 const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <div className="flex flex-row h-[80%] w-[90%] md:w-[60%] relative">
-        <div className="flex flex-col items-start justify-between p-4 text-white bg-black opacity-75 rounded-3xl w-full">
+        <div className="flex flex-col items-start justify-between p-4 text-white bg-black opacity-0 md:opacity-75 rounded-3xl w-full">
           <h3>Getting Started with Pillar.ai</h3>
           <div className="max-w-[50%]">
             <h2 className="p-5">Our Friends</h2>
@@ -28,17 +31,19 @@ const Login = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col text-white bg-black opacity-100 rounded-3xl absolute right-0 h-full w-[100%] md:w-[50%]">
-          <h3 className="ml-5">Login</h3>
-          <div className="flex flex-row justify-center items-center w-full">
-            <button className="border-2 border-white text-center rounded-lg p-2 m-1">
+        <div className="flex flex-col justify-center text-white bg-black opacity-80 rounded-3xl absolute right-0 h-full w-[100%] md:w-[50%]">
+          <h3 className="ml-10 mb-10 text-2xl font-bold">Login</h3>
+          <div className="flex flex-row justify-evenly items-center w-full">
+            <button className="border-2 border-white text-center rounded-lg px-2 py-3 m-1 flex flex-row items-center">
+              <img src={GoogleIcon} className="w-5 mr-2" />
               Login with Google
             </button>
-            <button className="border-2 border-white text-center rounded-lg p-2 m-1">
+            <button className="border-2 border-white text-center rounded-lg px-2 py-3 m-1 flex flex-row items-center">
+            <img src={FacebookIcon} className="w-5 mr-2" />
               Login with Facebook
             </button>
           </div>
-          <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col items-center justify-center my-10">
             <p>-OR-</p>
           </div>
           <div className="flex flex-col items-center justify-center">
