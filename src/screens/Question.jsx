273fcb80ee5ddc2b994f5af90a4289c636bd1ed5
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-05-25 11:26:16 +0530

    feat: Add Google Fonts link to index.html and update font styles

--- src/screens/Question.jsx
+++ src/screens/Question.jsx
@@ -52,8 +52,8 @@ const Question = () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="flex flex-col h-[80%] w-[90%] bg-black bg-opacity-75 rounded-3xl text-green-500 relative">
-        <div className="flex flex-col items-start justify-between py-5 px-10">
+      <div className="flex flex-col h-[80%] w-[90%] bg-black bg-opacity-75 rounded-3xl text-green-500 relative font-courier ">
+        <div className="flex flex-col items-start justify-between py-5 px-10 overflow-y-scroll hide-scrollbar">
           <h3 className="text-2xl">
             1 -&gt; We are excited for you to join the Pillar family. Before we
             begin it's important we gather as much information about you/your
@@ -64,8 +64,13 @@ const Question = () => {
           <TextInput label="Email" placeholder="abc@aabc.lk" type="email" />
           <TextInput label="Phone number" placeholder="0712345678" />
           <TextInput label="Instagram handle" placeholder="@jane.smith" />
+          <TextInput label="First name" placeholder="Jane" />
+          <TextInput label="Last name" placeholder="Smith" />
+          <TextInput label="Email" placeholder="abc@aabc.lk" type="email" />
+          <TextInput label="Phone number" placeholder="0712345678" />
+          <TextInput label="Instagram handle" placeholder="@jane.smith" />
         </div>
-        <div className="flex flex-row absolute right-0 bottom-0 mr-5 mb-10">
+        <div className="flex flex-row right-0 bottom-0 mr-5 mb-10 absolute">
           <button className="bg-green-500 border-r-2 p-2 rounded-l-lg">
             <img src={UpIcon} className="w-7" />
           </button>
