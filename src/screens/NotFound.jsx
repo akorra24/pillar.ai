fe532c7baf69b47897266c642f987f8c894ebf72
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-05-29 23:39:23 +0530

    page not found page

--- src/screens/NotFound.jsx
+++ src/screens/NotFound.jsx
@@ -3,16 +3,17 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-4">Page Not Found</h1>
-      <p className="text-lg text-gray-500 mb-8">
-        Oops! The page you're looking for doesn't exist.
-      </p>
-      <Link
-        to="/"
-        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300"
-      >
-        Go Home
-      </Link>
+      <div className="flex flex-col h-[80%] w-[90%] md:w-[60%] bg-black bg-opacity-75 rounded-3xl text-white justify-center items-center">
+        <h1 className="text-5xl font-bold mt-10">404</h1>
+        <p className="text-2xl mt-4">Page Not Found</p>
+        <p className="mt-4">The page you are looking for does not exist.</p>
+        <Link
+          to="/"
+          className="mt-10 bg-green-500 text-white py-2 px-4 rounded"
+        >
+          Go Home
+        </Link>
+      </div>
     </div>
   );
 };
